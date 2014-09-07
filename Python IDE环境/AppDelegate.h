e1849a1627c293721f66f59d946150b6aa91e6a7
teaxus <teaxus@teaxusdeMacBook-Pro.local>
2014-09-07 22:25:13 +0800

    Initial Commit

---
+++ Python IDE环境/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Python IDE环境
+//
+//  Created by teaxus on 14-9-7.
+//  Copyright (c) 2014年 teaxus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Python IDE环境Tests/Python_IDE__Tests.m
@@ -0,0 +1,34 @@
+//
+//  Python_IDE__Tests.m
+//  Python IDE环境Tests
+//
+//  Created by teaxus on 14-9-7.
+//  Copyright (c) 2014年 teaxus. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Python_IDE__Tests : XCTestCase
+
+@end
+
+@implementation Python_IDE__Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
